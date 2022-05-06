3c3a61ab6c1ff2ab3b10c079a067824d9cb84653
Manmohan Singh <pingmanmohan@gmail.com>
2022-05-06 11:54:57 +0530

    Repo initialised + basic setup

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+
+// init app
+const app = express();
+
+// add middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// basic route
+app.get("/", (req, res): void => {
+  res.status(200).json({ msg: `Server is running` });
+});
+
+// 404 api
+app.use((req, res) => {
+  res.status(404).json({ msg: "Page not found" });
+});
+
+app.listen(5000, () => console.log(`Server started on port 5000`));
